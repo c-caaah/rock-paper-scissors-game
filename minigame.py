34b21900dce1d56c0d15df1a5a3d5c71c637f92f
Carol <carolinedacosta.c@gmail.com>
2021-10-03 01:39:30 -0300

    fix code

--- minigame.py
+++ minigame.py
@@ -3,7 +3,7 @@ from random import choice
 comp_victories = 0
 player_victories = 0
 
-def 0option_player():
+def Option_player():
     choose_player = input("Choose Rock, Paper or Scissors: ")
     if choose_player in ["Scissors", "SCISSORS", "scissors"]:
         choose_player = "scissors"
