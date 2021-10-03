1b5c7ca3de3f622f86d075edef2ac1a4db01fe06
Carol <carolinedacosta.c@gmail.com>
2021-10-03 01:50:12 -0300

    add add minigame start code

--- minigame.py
+++ minigame.py
@@ -6,5 +6,8 @@ player_victories = 0
 def Option_player():
     choose_player = input("Choose Rock, Paper or Scissors: ")
     if choose_player in ["Scissors", "SCISSORS", "scissors"]:
-        choose_player = "scissors"
-    elif 
+        choose_player = "Scissors"
+    elif choose_player in ["Rock", "ROCK", "rock"]:
+        choose_player = "Rock"
+    elif choose_player = ["Paper", "PAPER", "paper"]:
+        choose_player = "Paper"
